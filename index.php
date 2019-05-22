5269117ca5c550941234c14bbc635a78dc54e013
StarshipladDev <StarshipladDev@gmail.com>
2019-05-22 15:20:26 +1200

    Add files via upload

--- index.php
+++ index.php
@@ -1,28 +1,31 @@
-<--/http://www.starshiplad.com/sendit.php?name=SQL+injection&info=This+will+be+test+data !-->
+
 <!DOCTYPE HTML>
 <html>
 <head>
+<!--Import header via php-->
 <?php
 	include"header.php";
 ?>
 </head>
-
+<!--Main section of content  -->
 <body>
 
 <div id="main">
 <main>
 <div class="readPanel">
-	Welcome to Starshiplad.com! This webpage is designed primarily to show off CSS, PHP,SQL and HTML design and code learning I do while developing
-	projects throughout the year that you can see on my <a href="https://github.com/StarshipladDev">github </a>.<br> 
-	This frontpage is to guide users through the webiste. To input data into a database stored on the server go to the "Input data" tab, where unimportant details
-	can be entred into a database's SQL table called "importantThings", where my <a href="https://github.com/StarshipladDev/Hack-Tool">hack tool </a> will
-	hopefully be able to retreive the given data
+	<der>Welcome!</der><br>
+	Welcome to Starshiplad.com! This webpage is designed primarily to show off CSS, PHP,SQL and HTML design and code learning that I do while developing
+	projects throughout the year. You can check out my <a href="https://github.com/StarshipladDev">github </a> for more info.<br> 
+	This frontpage is to guide users through the website. To input data into a database stored on the server go to the "Input data" tab, where unimportant details
+	can be entred into a database's SQL table called "importantThings". This data will be apssword protected, but accesible via my<a href="https://github.com/StarshipladDev/Hack-Tool">hack tool </a> I developed.
 </div>
 <div class="readPanel">
-	If you came to check the website out from the twitter, thankyou very much! Any ideas for stuff I should try to learn to code or any advice, don't hesitate to 
+	<der>Hi Twitter </der><br>
+	If you came to check the website out from my twitter, thankyou very much! Any ideas for stuff I should try to learn to code or any advice, don't hesitate to 
 	DM me! Always cool to talk to other programmers out there :)
 </div>
 <div class="readPanel">
+	<der>Important Info Search </der><br>
 	The <a href="http://www.starshiplad.com/search.php">'Search Names' </a> tab can be used to search the database created with 'input info. This will return a table of matching 
 	data entries. It is also the primary penetration point with my hack tool
 	<br><br>Filler textto fill up the last panel to see how good it looks<p>
@@ -34,6 +37,16 @@ I have a record of striving to understand more complex aspects of the roles I ha
 My ideal position would be a role that allows me to achieve practical solutions to open ended problems with technical skills, by being able to co-design solutions with other members of the organization. An environment of open communication that allows the learning and development of further skills would be my preferred setting.
 
 
+</div>
+<div class="readPanel">
+<der>Planned Future Projects </der><br>
+I'm hoping to do get the following projects up on my Github:<br>
+<ul>
+<li>More proffesional documentation for current projects</li>
+<li>A simple MTG life counter android app</li>
+<li>A react.js applet</li>
+<li>More design-oriented C# froms/WPF</li>
+</ul>
 </div>
 </main>
 
