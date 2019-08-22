dc4fcf4186ba50b59e7443b23976b848d4f4ed9b
StarshipladDev <StarshipladDev@gmail.com>
2019-08-22 16:21:09 +1200

    Proof Update
    Added a description of proof and cleaner hack proof picture

--- proof.php
+++ proof.php
@@ -6,6 +6,11 @@
 ?>
 </head>
 <body>
+<div class="readPanel">
+Below is an image dispalying the regular output of the 'output data' page compared to the valeus retreived from my SQL injection tool.
+On the left you can see important info is hidden, however using UNION statments in SQL,the  <a href="https://github.com/StarshipladDev/Hack-Tool" >hack tool</a> dispalys this data in
+non-password slots, reads them by using Regex to search for a prefix and suffix, and the nconcats nn-hidden data with the passwrod protected data
+</div>
 <img src="http://www.starshiplad.com/proof.JPG" alt="Proof of hack">
 </body>
 <footer>
