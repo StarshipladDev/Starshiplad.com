8137b81ca0524261abe6bdae662e6433ddc98539
StarshipladDev <StarshipladDev@gmail.com>
2020-05-03 22:11:45 +1200

    New Page color-scheme testing
    Added a test-case index with a more 'blocky' style.
    Attatched to new .css file.
    Add new Pallete color scheme

---
+++ Testindex.php
@@ -0,0 +1,102 @@
+<!DOCTYPE HTML5>
+<html>
+<head>
+
+<Title>The Home Of Starshiplad!</Title>
+<link rel="icon" type="image/png" href="http://www.starshiplad.com/favicon.png"/>
+<link rel="stylesheet" href="https://www.starshiplad.com/NewStyle.css" >
+</head>
+<body>
+
+<div id="titleBar">
+<a href= "/index.php">  <img src="/logo.png" alt="Starshiplad.Com"></a>
+<div class="menuItem"><a href="http://www.starshiplad.com/index.php">Home</a></div>
+<div class="menuItem"><a href="https://github.com/StarshipladDev">Projects</a></div>
+<div class="menuItem"><a href="http://www.starshiplad.com/input.php">Input Info</a></div>
+<div class="hiddenOver menuItem" ><a href="http://www.starshiplad.com/output.php">Output Info</a><div class="hiddenUnder menuitem"><a href="http://www.starshiplad.com/search.php">Search Names</a> </div> </div>
+<div class="menuItem"><a href="http://www.starshiplad.com/proof.php">Hack Proof</a></div>
+<div class="menuItem"><a href="http://www.starshiplad.com/roleplayinggames.php">RPGs</a></div>
+</div>
+<div id="titleStart">
+
+</div>
+<div class="section">
+	<div class="fullPanel Orange">
+		<der>Welcome!</der><br>
+		Welcome to Starshiplad.com! This webpage is designed primarily to show off CSS, PHP,SQL and HTML design and code learning that I do while developing
+		projects throughout the year. You can check out my <a href="https://github.com/StarshipladDev">github </a> for more info.<br> 
+		This front page is to guide users through the website. To input data into a database stored on the server go to the "Input data" tab, where unimportant details
+		can be entered into a database's SQL table called "importantThings". This data will be password protected, but accessible via my <a href="https://github.com/StarshipladDev/Hack-Tool">hack tool </a> I developed.
+
+	</div>
+</div>
+<div class="section">
+	<div class="halfPanel Orange">
+		<der>Hi Twitter </der><br>
+		If you came to check the website out from my twitter, thank you very much! Any ideas for stuff I should try to learn to code or any advice, don't hesitate to 
+		DM me! Always cool to talk to other programmers out there :)
+		<br>
+		<div class="twitter-feed">
+			<a class="twitter-timeline"  data-height="500" data-width="330" data-theme="dark" href="https://twitter.com/StarshipladDevp">Tweets by StarshipladDevp</a>
+		</div>
+		<script async src="https://platform.twitter.com/widgets.js" charset="utf-8">
+
+		</script>
+	</div>
+	<div class="halfPanel Black">
+		<der>Important Info Search </der><br>
+		The <a href="http://www.starshiplad.com/search.php">'Search Names' </a> tab can be used to search the database created with 'input info. This will return a table of matching 
+		data entries. It is also the primary penetration point with my hack tool
+		<br><br>Filler text to fill up the last panel to see how good it looks<p>
+	</div>
+</div>
+<div class="section">
+	<div class="halfPanel Black">
+		<der>Contact </der><br>
+		Send me a message through the below form to get in contact about anything!
+		<form action="email.php" method="post">
+
+		Message: <textarea id="InputText" rows="3" cols="40" name="Message"></textarea><br>
+		Name: <input type="text" name="Name"><br>
+
+		<input type="submit">
+	</div>
+	<div class="halfPanel Orange">
+		<der>Current Projects </der><br>
+		I am constantly finding new programming solutions to try out. <br>
+		If there are any cool ideas you'd like to see tried out, feel free to message me with the contact box above.<br>
+		Current projects include:<br>
+		<a href="https://github.com/StarshipladDev/FiveMoveMurder">A C#/SQL RPG Managment Tool </a><br>
+		<a href="https://Starshiplad.com/AirAToB.php">A javascript / Google API /PHP Mapping Website </a><br>
+		<a href="https://github.com/StarshipladDev/MTGHealthCounter">A Java/XML Android companion app for MTG</a><br>
+		<a href="https://github.com/StarshipladDev/LIC">A javascript/HTML Google Chrome Extension</a><br>
+		<a href="https://github.com/StarshipladDev/Hack-Tool">A .NET / MySQL database and automated SQLi tool to interact with it</a><br>
+	
+	</div>
+</div>
+<div class="section">
+	<div class="fullPanel Orange">
+		<der>Planned Future Projects </der><br>
+		I'm hoping to do get the following projects up on my Github:<br>
+		<ul>
+		<li>More professional documentation for current projects</li>
+		<li>A simple MTG life counter android app  <i>Currenty in progress , the APK can be downloaded <a href="https://Starshiplad.com/MTGApp.apk"> here </a> </i></li>
+		<li>An openGL card Game <i>Currenty in progress, see <a href="https://Starshiplad.com/openGL.php"   >https://Starshiplad.com/openGL.php </a></i></li>
+		<li>A Angular.js applet</li>
+		<li>More design-oriented C# froms/WPF <i>Currenty in progress </i></li>
+	</div>
+</div>
+<div class="section">
+	<div class="halfPanel Black">
+		<img src="http://www.starshiplad.com/GunRun.PNG">
+	</div>
+	<div class="halfPanel Orange">
+		<p>Man this looks nice. </p>
+		<p>It works for lots of lines too</p>
+		<p>See</p>
+		<p>it just keeps the same </p>
+	</div>
+</div>
+
+</body>
+</html>
