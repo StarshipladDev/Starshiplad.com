77257eed4b5f837672d7500736099a2afba97767
StarshipladDev <StarshipladDev@gmail.com>
2021-03-03 23:25:21 +1300

    03/03/2021 Add Crypto GUI
    TODO : 
    Add proper Changelog, 
    update README, 
    delete Refract Branch. 
    Sum Up: Added GUI, Testing/Crypto page , error handling, '<' and '>' character handling, interactive unhash dispaly handling and CSS work.

--- Testing/Crypto/JavascriptTestingCrypto.js
+++ Testing/Crypto/JavascriptTestingCrypto.js
@@ -1,3 +1,327 @@
+<<<<<<< HEAD
+
+
+/*
+Let's say the limit is 6.
+A =1, B=2, C=3, D=4, E=5 , F=6
+After Hash function, it mods each Letter into a double letter.
+The first letter is a numerical representation of how many times mods the hash function did.
+E.G- 
+hashing 'A' above if the hash function added Value +3:
+A -> Hashfunction -> 0E
+The '0' is due to A('1') + A('1')+3 is 5 ('E'),thus not hitting the end of the possible answers (A - E)
+
+Now compare to if we hash the letter 'E':
+E -> Hashfunction -> 2F
+The '2' is due to the hasFunction looping through the possible answers twice
+
+To unhash  : (Total Number (First digit * value of second digit)  - equations additions )/(multiplications);
+
+
+*/
+function Write(message,tag="default"){
+	var date = new Date();
+	console.log(date.getUTCHours()+":"+date.getUTCMinutes()+":"+date.getUTCSeconds()+":"+date.getUTCMilliseconds()+"->"+tag+"->"+message);
+}
+
+function HashString(input){
+	
+	var s = input.split('');
+	var returnString="";
+	for(var i=0 ;i<s.length;i++){
+		returnString+=HashEquation(s[i].charCodeAt(0)-32);
+	}
+	if(returnString==""){
+		returnString  = " ";
+	}
+	Write("HH--"+returnString+"--");
+	return returnString;
+	
+}
+
+function HashEquation(inputKey,times=3){
+	var tag= "HashEquation"
+	Write("\n inputKey is "+inputKey+"("+String.fromCharCode(inputKey+32)+")"+" times is "+times,"HashEquation");
+	var mod=92;
+	
+	Write("Pre hash inputKey is "+inputKey,"HashEquation");
+	var inp = inputKey;
+	inputKey= inputKey*times+24+7-6;
+	var rounds=   Math.floor(inputKey/mod);
+	if(rounds>9){
+		rounds=(String.fromCharCode((rounds%mod)+32));
+		if(rounds=='<'){
+		Write("since rounds code is <, swapping < for "+ String.fromCharCode(parseInt(93) +32) ,tag);
+		rounds = String.fromCharCode(32+93);
+		}
+		if(rounds=='>'){
+			Write("since rounds code is >, swapping > for "+ String.fromCharCode(parseInt(94) +32),tag );
+			rounds = String.fromCharCode(32+94);
+		}
+	}
+	Write("After hash inputKey is "+inputKey+" & rounds are "+rounds+". inputKey %"+mod+" is "+(inputKey%mod),"HashEquation");
+	var letterCode=inputKey % mod;
+	Write("rounds/letterCode was "+rounds +"/" +letterCode,"HashEquation");
+	if(letterCode<10){
+		letterCode="0"+letterCode;
+	}
+	if(letterCode==28){
+		Write("since letter code is <, swapping < for "+ String.fromCharCode(parseInt(93) +32) ,tag);
+		letterCode = 93;
+	}
+	if(letterCode==30){
+		Write("since letter code is >, swapping > for "+ String.fromCharCode(parseInt(94) +32),tag );
+		letterCode = 94;
+	}
+	letterCode=String.fromCharCode(parseInt(letterCode) +32);
+	
+	Write("rounds/letterCode is "+rounds +"/" +letterCode,"HashEquation");
+	return ""+rounds+letterCode;
+	
+}
+
+
+function Unhash(input){
+	var s = input.split('');
+	var returnString="";
+	for(var i=0 ;i<s.length-1;i+=2){
+		returnString+=UnHashEquation(s[i]+s[i+1]);
+		Write("UnhashedString is "+returnString.charAt(i)+" the end of "+returnString,"UnHashOverall");
+	}
+	Write("UH|"+returnString+"|");
+	return returnString;
+}
+function UnHashEquation(inputKey,mod=92){
+	var tag="UnHashEquation";
+	Write("\n inputKey is "+inputKey);
+	var rounds = inputKey.split("")[0];
+	var number = inputKey.split("")[1].charCodeAt(0) -32;
+	if(number==93){
+		number="<".charCodeAt(0) -32;
+		Write("Since number = "+String.fromCharCode(93+32)+" changing number to <",tag);
+	}
+	if(number==94){
+		Write("Since number = "+String.fromCharCode(94+32)+" changing number to >",tag);
+		number=">".charCodeAt(0) -32;
+	}
+	Write("Step 0) Number is " +number+" and mod is "+mod+" and rounds are "+rounds,tag);
+	number = number + (mod * rounds);
+	Write("Step 1) - Add mod*rounds Number is " +number,tag);
+	number = number-24-7+6;
+	Write("Step 2)-Perform simple Addition Number is " +number,tag);
+	number = number/3;
+	Write("Step 3)-Divide number by 3 Number is " +number +" /// "+String.fromCharCode(number+32) ,tag);
+	if(number=="<".charCodeAt(0) -32){
+		Write("Changed nubmer as it is <",tag);
+		number=93;
+	}
+	if(number==">".charCodeAt(0) -32){
+			Write("Changed nubmer as it is >",tag);
+		number=94;
+	}
+	
+	return String.fromCharCode(32+(number));
+	
+}
+
+function GetPublicUsernameButton(){
+	
+	var username = document.getElementById('inputTextUsername').value;
+	var password = document.getElementById("inputTextPassword").value;
+	document.getElementById('publicUsernameDisplay').innerHTML=GetPublicUsername(password,username);
+}
+
+function GetPublicUsername(password,username){
+	var tag="GetPublicUserName()";
+	var mod=0;
+	var publicUsername = "";
+	var tempUserName ="          ".split(""); //10 char
+	for(var i=0 ; i< password.length;i++){
+		mod+=password.charCodeAt(0);
+	}
+	
+	Write("temp username and user name are now "+tempUserName +" & "+username,tag);
+	for(var f=0; f<tempUserName.length;f++){
+		if(f<username.length){
+			tempUserName[f]=String.fromCharCode(username.charCodeAt(f));
+		}
+		//Write("temp username and user name are now "+tempUserName +" & "+username,tag);
+
+	}
+	for(var f=tempUserName.length; f<username.length-tempUserName.length;f++){
+		tempUserName[f%tempUserName.length]=String.fromCharCode((((username.charCodeAt(f%tempUserName.length)+username.charCodeAt(f))/2)+32)%95);
+		
+			//Write("temp username and user name are now "+tempUserName +" & "+username,tag);
+
+	}
+	username=tempUserName.join("");
+	Write("temp username and user name are now "+tempUserName +" & "+username,tag);
+
+	for(var i=0 ;i<username.length;i++){
+		publicUsername+=HashEquation(username[i].charCodeAt(0)-32,mod);
+	}
+	Write("post Hash Username is "+publicUsername,tag);
+	for(var i=0; i< publicUsername.length;i++){
+		Write("Key is "+publicUsername.charAt(i)+" // "+publicUsername.charCodeAt(i),tag);
+	}
+	Write("PublicUsername is "+publicUsername,tag);
+	return publicUsername;
+}
+
+
+
+function AddTransactionButton(){
+	var to = document.getElementById('inputTextTo').value;
+	var ammount = document.getElementById('inputTextAmmount').value;
+	var username = document.getElementById('inputTextUsername').value;
+	var password = document.getElementById("inputTextPassword").value;
+	if(to.length!=20){
+		document.getElementById("title").innerHTML="'To' must be 20 characers long";
+	}
+	else{
+		AddTransaction(to,ammount,username,password);
+	}
+	
+}
+function AddTransaction(to,ammount,username,password){
+	var username = document.getElementById('inputTextUsername').value;
+	var password = document.getElementById("inputTextPassword").value;
+	var pubUsername=GetPublicUsername(password,username);
+	var id= parseInt(document.getElementById('indexHidden').innerHTML);
+	var date = new Date();
+	var day = date.getUTCDay();
+	if(day<10){
+		day="0"+day;
+	}
+	var month =date.getUTCMonth();
+	if(month<10){
+		month="0"+month;
+	}
+	var timeStamp = day+"/"+month+"/"+date.getUTCFullYear();
+	var priorHash = document.getElementById('dataHidden').innerHTML;
+	var data = pubUsername+"Give"+ammount+to;
+	
+	let newBlock = new Block(id,priorHash,timeStamp,data);
+	document.getElementById('indexHidden').innerHTML = "";
+	if(id<9){
+		document.getElementById('indexHidden').innerHTML += "0";
+	}
+	if(id<99){
+		document.getElementById('indexHidden').innerHTML += "0";
+	}
+	document.getElementById('indexHidden').innerHTML +=(id+1);
+	document.getElementById('dataHidden').innerHTML = newBlock.hash;
+	
+}
+
+
+function hashSample(){
+	var input =document.getElementById('inputText').value;
+	var output = document.getElementById('outputText').innerHTML;
+	Write("hashSampleHit with "+input +" and "+output);
+	if(output != null && input != null){
+		document.getElementById('outputText').innerHTML=HashString(input);
+	}
+	else{
+		document.getElementById('outputText').innerHTML=" ";
+
+	}
+	
+}
+function unhashSample(){
+	try{
+		var input =document.getElementById('inputTextUnhash').value;
+		var output = document.getElementById('outputTextUnhash').innerHTML;
+		Write("unhashSample with "+input +" and "+output);
+		if(output != null && input != null){
+			var outputText = Unhash(input);
+			
+			document.getElementById('outputTextUnhash').innerHTML=outputText;
+			if(outputText==null || outputText.charCodeAt(0)>(95+32) || outputText.charCodeAt(0)<(32)){
+				document.getElementById('outputTextUnhash').innerHTML="Bad Hash";
+			}
+
+		}
+	}
+	catch(er){
+		document.getElementById('outputTextUnhash').innerHTML="Bad Hash";
+	}
+	
+	
+	
+}
+function ShowCryptoButton(){
+	if(parseInt(document.getElementById('indexHidden').innerHTML)>0){
+		var title = document.getElementById('title');
+		title.style.fontSize="100%";
+		title.innerHTML=document.getElementById('dataHidden').innerHTML;
+		document.getElementById('unhashedHidden').innerHTML=document.getElementById('indexHidden').innerHTML;
+	}
+	
+}
+function UnhashCryptoButton(){
+	var title = document.getElementById('title');
+	var unhashed;
+	var overallCount =parseInt(document.getElementById('indexHidden').innerHTML);
+	var unhashedCount =parseInt(document.getElementById('unhashedHidden').innerHTML);
+	
+	if(unhashedCount>0){
+		if(unhashedCount ==overallCount){
+			title.innerHTML="";
+		}
+		var writtenText=document.getElementById('dataHidden').innerHTML;
+		//20 per username, 3 per index, 10 per timestamp, give 4 , ammoun 1 58
+		for(var i=0;i<(overallCount-unhashedCount)+1;i++){
+			writtenText=Unhash(writtenText.substr((i)*58))
+		}
+		title.innerHTML+=((unhashedCount))+")<a style='color:#b30000'> \< Transaction # ||</a>"+writtenText.substr(0,60)+" \< <a style='color:#b30000'> Data Block ||</a>"+writtenText.substr(60)+"<a style='color:#b30000'>|| \< Prior Blocks (Enrypted)</a><br> <br>";
+		document.getElementById('unhashedHidden').innerHTML = unhashedCount-1;
+	}
+}
+
+function runTest(){
+	document.getElementById("test").innerHTML="Check Browser Console";
+	document.getElementById("test").style.color="red";
+	var block1 = new Block(0,'',"24/02/2021","give 3");
+	Write("Made Block 1");
+	Write("Block1 has the following data" +block1.index+" "+block1.timeStamp+" "+block1.data+" "+block1.priorHash);
+	Write("Block1 has hash "+block1.hash);
+	Write("Block1 unhashed is  "+ Unhash(block1.hash));
+
+	Write("----------------------");
+
+	var block2 = new Block(1,block1.hash,"24/02/2021","give 10");
+	Write("Made Block 2");
+	Write("Block2 has the following data" +block2.index+" "+block2.timeStamp+" "+block2.data+" "+block2.priorHash);
+	Write("Block2 has priorhash "+block2.priorHash);
+	Write("Block2 has hash "+block2.hash);
+	Write("Block2 has unhashed hash value of  "+Unhash(block2.hash));
+	
+Write("Block2's prior block has an unhash of "+Unhash(block2.priorHash));
+}
+
+for(var i=0; i<96;i++){
+	Write("Character "+i+"("+(i+32)+")"+ " is "+String.fromCharCode(i+32));
+}
+Write("Starting");
+hashSample();
+
+class Block{
+	constructor(index,priorHash,timeStamp,data,onesNeeded=0){
+		this.index=index;
+		this.priorHash=priorHash;
+		this.timeStamp=timeStamp;
+		this.onesNeeded=onesNeeded;
+		this.data=data;
+		this.hash=this.HashFunction(this.index,this.timeStamp,this.data,this.priorHash,this.onesNeeded);
+		
+	}
+	HashFunction(index,timestamp,data,priorHash,onesNeeded=0){
+		Write("Starting with index "+index);
+		return HashString(index+timestamp+data+priorHash);
+	}
+}
+=======
 /*
 Let's say the limit is 6.
 A =1, B=2, C=3, D=4, E=5 , F=6
@@ -154,3 +478,4 @@ function runTest(){
 }
 
 hashSample();
+>>>>>>> 245dd29f0106855205e3dcb434ed4e06f8ec0e18
